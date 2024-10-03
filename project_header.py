1187b1698cf6d108062a1a6deb508e78860d6a48
Dane Sperling <danesperling@gmail.com>
2024-10-02 19:14:55 -0500

    Initialized project

---
+++ project_header.py
@@ -0,0 +1,10 @@
+
+
+class choices():
+    def checkChoice(str):
+        pass
+    
+    def addChoice(str):
+        pass
+
+    
---
+++ project_implementation.py
@@ -0,0 +1,8 @@
+import project_header as head
+
+class choices(head.choices):
+    def checkChoice(str):
+        pass
+    
+    def addChoice(str):
+        pass
